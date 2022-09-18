09de89fe117fc926e8e634c1cdc520238bfa50f5
Fredy Papa <fredy.luis.wolf@gmail.com>
2022-09-18 09:15:06 -0500

    Desafío Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MenuComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
